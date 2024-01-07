38ca46790dd6d859b6bc31adba39ddcfc8d0fe78
victortads <vgs8@discente.ifpe.edu.br>
2024-01-06 21:48:47 -0300

    Leitura de comentários

--- js/cards.js
+++ js/cards.js
@@ -1,42 +1,32 @@
 import getToken from "./token.js";
+import coments from "./comentários.js";
 
 const cards = {
     addCardsOption: async function () {
-        btn_lista = Array.from(document.getElementsByClassName("adicionarLista"));
-        btn_lista.forEach(element => {
-            element.addEventListener("click", (event) => {
-                event.stopPropagation()
-                if (document.getElementById("div-adicionar-lista").classList.contains("displayNone")) {
-                    document.getElementById("div-adicionar-lista").classList.remove("displayNone");
-                    document.getElementById("div-adicionar-lista").classList.add("displayOn");
-                } else {
-                    document.getElementById("div-adicionar-lista").classList.remove("displayOn");
-                    document.getElementById("div-adicionar-lista").classList.add("displayNone");
-                }
-
-                document.getElementById("input-lista-id").value = event.target.parentNode.parentNode.id;
-
-                console.log(event.target.parentNode.parentNode.id)
-            });
-        });
+        
     },
     addCards: async function (list_id) {
         let cards = await this.getCards(getToken(), list_id);
         // console.log(cards);
         let cardsContent = "<ul>";
+        let IDs = [];
         cards.forEach((cards => {
-            cardsContent += `<li card_id="${cards.id}">${cards.name}</li>`;
-
+            cardsContent += `<li card_id="${cards.id}" class="cards-format">${cards.name}</li>`;
+            IDs.push(cards.id);
         }));
 
         cardsContent += "</ul>";
 
-
         
         const listsFormatElement = document.querySelector(`[list_id="${list_id}"]`);
-
+        
         if (listsFormatElement && listsFormatElement.classList.contains("lists-format")) {
             listsFormatElement.innerHTML += cardsContent;
+        }
+        
+
+        for (let i = 0; i < IDs.length; i++) {
+            await coments.addComments(IDs[i]);
         }
         return cardsContent;
 
---
+++ js/comentários.js
@@ -0,0 +1,44 @@
+import getToken from "./token.js";
+
+const coments = {
+    addComments: async function (card_id) {
+        let comments = await this.getComments(getToken(), card_id);
+        // console.log(cards);
+        let commentContent = "<ul>";
+        comments.forEach((comment => {
+            commentContent += `<li comment_id="${comment.id}" class="comment-format">${comment.comment}</li>`;
+
+        }));
+
+        commentContent += "</ul>";
+
+
+        
+        const cardsFormatElement = document.querySelector(`[card_id="${card_id}"]`);
+
+        if (cardsFormatElement && cardsFormatElement.classList.contains("cards-format")) {
+            cardsFormatElement.innerHTML += commentContent;
+        }
+        return commentContent;
+
+    },
+    getComments: async function (token, card_id) {
+        try {
+            const response = await fetch(`http://localhost:8087/api/v1/cards/${card_id}/comments`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer" + ` ${token}`,
+                },
+            });
+
+            const result = await response.json();
+            // console.log("Success: ", result);
+            return result;
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+}
+
+export default coments;
